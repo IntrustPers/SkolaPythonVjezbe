063734e6a5628b643b6d5f5463e594be5a8709ae
Bosko <officialintrust@gmail.com>
2025-05-16 10:00:43 +0200

    Update main.py

--- Bilježnica/main.py
+++ Bilježnica/main.py
@@ -1,1 +1,18 @@
+def prikazi_izbornik():
+    print("--- Jednostavna Bilježnica ---")
+    print("1. Dodaj novu bilješku")
+    print("2. Pregledaj sve bilješke")
+    print("3. Izlaz")
+    print("Odaberite opciju (1-3): ")
 
+def dodaj_biljesku():
+    unos = input("Unesi bilješku: ")
+    with open("biljeske.txt", "a") as biljeznica:
+        biljeznica.write(f"{unos}\n")
+        biljeznica.close()
+    print("Bilješka je uspješno spremljena.")
+
+def otvori_biljeske():
+    with open("biljeske.txt", "r") as biljeznica:
+        sadrzaj = biljeznica.read()
+        print(sadrzaj)
